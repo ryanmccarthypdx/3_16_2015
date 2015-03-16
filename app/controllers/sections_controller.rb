c59a084e86834e089a08afc5db25031cd477672a
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-16 11:19:53 -0700

    Add ability to add new sections and link to individual section pages

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,4 +1,19 @@
 class SectionsController < ApplicationController
+  def create
+    @section = Section.new(section_params)
+    if @section.save
+      flash[:notice] = "Section successfully added!"
+      redirect_to sections_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @section = Section.new
+    render :new
+  end
+
   def index
     @sections = Section.all
     render :index
@@ -7,5 +22,10 @@ class SectionsController < ApplicationController
   def show
     @section = Section.find(params[:id])
     render :show
+  end
+
+  private
+  def section_params
+    params.require(:section).permit(:name)
   end
 end
